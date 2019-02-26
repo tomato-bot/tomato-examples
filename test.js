4b097cc62eeac2b6776f7342b9e7d9991e3b0cb8
Moshe Zada <zada.moshe@gmail.com>
2019-02-26 16:27:14 +0200

    Reduce some errors

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var assert = require('assert');
-describe('HelloWorld Module', function() {
-  it('should return -1 when "Hello" is missing', function() {
-    assert.equal(0, "Hallo World".indexOf("Hello"));
+const assert = require('assert')
+
+describe('HelloWorld Module', () => {
+  it('should return -1 when "Hello" is missing', function () {
+    assert.equal(0, 'Hallo World'.indexOf('Hello'));
   });
-  it('should return 0 when sentence starts with Hello', function() {
-    assert.equal(0, "Hello World, how are you?".indexOf("Hello"));
+  it('should return 0 when sentence starts with Hello', () => {
+    assert.equal(0, 'Hello World, how are you?'.indexOf('Hello'));
   });
 });
