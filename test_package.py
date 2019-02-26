dab25abea60601892dfa34fb93e98485386383be
Moshe Zada <zada.moshe@gmail.com>
2019-02-26 13:46:23 +0200

    Add python example

---
+++ test_package.py
@@ -0,0 +1,40 @@
+import sys
+from unittest import SkipTest
+
+
+def test_add():
+    assert 1 + 1 == 2
+
+
+def test_add2():
+    assert 1 + 1 == 2
+
+
+def test_add3():
+    assert 1 + 1 == 2
+    # fail in python 2 only
+    assert sys.version_info.major == 3
+
+
+def test_add4():
+    assert 1 + 1 == 2
+
+
+def test_add5():
+    assert 1 + 1 == 2
+
+
+def test_add6():
+    assert 1 + 1 == 2
+
+
+def test_add7():
+    assert 1 + 1 == 2
+    assert 1 + 1 == 2
+    assert 1 + 1 == 2
+    assert 1 + 1 == 3
+    assert 1 + 2 == 3
+
+
+def test_skip():
+    raise SkipTest('lol')
